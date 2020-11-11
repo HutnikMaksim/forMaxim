331904616ae9dd2e2d16156425f0b481daaa5363
Daddy <matrimaxim@gmail.com>
2020-11-11 21:18:43 +0300

    MiniTests

---
+++ src/by/it/hutnik/jd00_01_my_tests/IndexMassBody.java
@@ -0,0 +1,17 @@
+package by.it.hutnik.jd00_01_my_tests;
+
+import javax.swing.*;
+
+public class IndexMassBody {
+    public static void main(String[] args) {
+        double mass, heigh, bmi;
+        String res;
+        res = JOptionPane.showInputDialog("Ваш рост в метрах:");
+        heigh = Double.parseDouble(res);
+        res = JOptionPane.showInputDialog("Ваш вес в килограммах:");
+        mass = Double.parseDouble(res);
+        bmi = mass/heigh/heigh;
+        bmi = Math.round(bmi * 100)/100.0;
+        JOptionPane.showMessageDialog(null,"Ваш индекс массы тела:" + bmi);
+    }
+}
