ebdfafc9ffd6e96cabc9d0a065aff186ce5b123e
Daddy <matrimaxim@gmail.com>
2021-03-07 12:31:49 +0100

    7mar2021

---
+++ src/by/it/hutnik/javazadachi_com/Task_5_1.java
@@ -0,0 +1,18 @@
+package by.it.hutnik.javazadachi_com;
+
+import java.util.Scanner;
+
+/*
+1. Создать программу, которая будет сообщать, является ли целое число, введённое пользователем,
+чётным или нечётным. Если пользователь введёт не целое число, то сообщать ему об ошибке.
+ */
+public class Task_5_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNextInt()){
+            if(scanner.nextInt() % 2 == 0){
+                System.out.println("Введённое число чётное!");
+            } else System.out.println("Введённое число нечётное");
+        } else System.out.println("Введено некорректное значение");
+    }
+}
---
+++ src/by/it/hutnik/javazadachi_com/Task_5_2.java
@@ -0,0 +1,22 @@
+package by.it.hutnik.javazadachi_com;
+
+
+import java.util.Scanner;
+
+/*
+2. Создать программу, которая будет вычислять и выводить на экран сумму двух целых чисел, введённых пользователем.
+Если пользователь некорректно введёт хотя бы одно из чисел, то сообщать об ошибке.
+ */
+public class Task_5_2 {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        int a = 0, b = 0, c = 0;
+        if(scaner.hasNextInt()) {
+            a = scaner.nextInt();
+        } if(scaner.hasNextInt()) {
+            b = scaner.nextInt();
+            c = a + b;
+            System.out.println(c);
+        } else System.out.println("Enter error");
+    }
+}
