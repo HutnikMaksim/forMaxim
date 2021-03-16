c134178c5f33cbc8d86e205d86c8fe1074012832
Daddy <matrimaxim@gmail.com>
2021-03-16 19:17:46 +0100

    16mar2021

---
+++ src/by/it/hutnik/javazadachi_com/Task_6_14.java
@@ -0,0 +1,24 @@
+package by.it.hutnik.javazadachi_com;
+
+import java.util.Arrays;
+
+/*
+Создать двумерный массив из 8 строк по 5 столбцов в каждой из случайных целых чисел из отрезка [10;99].
+Вывести массив на экран.
+ */
+public class Task_6_14 {
+    public static void main(String[] args) {
+        int count = 0;
+        int row = 8;
+        int col = 5;
+        int[][] mass = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                mass[i][j] = (int)(Math.random() * 90 + 10);
+            }
+        }
+        for(int r[] : mass){
+            System.out.println(Arrays.toString(r));
+        }
+    }
+}
